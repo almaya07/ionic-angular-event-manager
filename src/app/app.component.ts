e23ba8b9234235e484f3942466ec9c33ab4b9937
Jorge Vergara <javebratt@gmail.com>
2019-05-20 20:37:11 -0500

    Initializes Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Plugins } from '@capacitor/core';
+import * as firebase from 'firebase/app';
+import { firebaseConfig } from './credentials';
 
 const { SplashScreen, StatusBar } = Plugins;
 
@@ -20,5 +22,6 @@ export class AppComponent {
     StatusBar.hide().catch(error => {
       console.error(error);
     });
+    firebase.initializeApp(firebaseConfig);
   }
 }
---
+++ src/app/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDnlvRQ4UJl9ypzQjcQkMfIe8i9yEJ4oe0',
+  authDomain: 'firestore-event-manager.firebaseapp.com',
+  databaseURL: 'https://firestore-event-manager.firebaseio.com',
+  projectId: 'firestore-event-manager',
+  storageBucket: 'firestore-event-manager.appspot.com',
+  messagingSenderId: '823274361576',
+  appId: '1:823274361576:web:96975e3016ca2c76'
+};
