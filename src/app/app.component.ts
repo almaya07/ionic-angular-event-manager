af9c2e4c0ac47873a0b164f9b4f43bfdd113062d
Jorge Vergara <javebratt@gmail.com>
2019-08-24 12:41:18 -0500

    Nuke everything and upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,27 @@
 import { Component } from '@angular/core';
-import { Plugins } from '@capacitor/core';
-import * as firebase from 'firebase/app';
-import { firebaseConfig } from './credentials';
 
-const { SplashScreen, StatusBar } = Plugins;
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor() {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    SplashScreen.hide().catch(error => {
-      console.error(error);
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
-
-    StatusBar.hide().catch(error => {
-      console.error(error);
-    });
-    firebase.initializeApp(firebaseConfig);
   }
 }
