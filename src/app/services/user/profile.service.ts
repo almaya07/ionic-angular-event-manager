03aa26eebf0808879ec287325a1162e8e813d7b8
Jorge Vergara <javebratt@gmail.com>
2019-08-25 06:41:44 -0500

    Adds the user profile service

--- src/app/services/user/profile.service.ts
+++ src/app/services/user/profile.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  public userProfile: firebase.firestore.DocumentReference;
+  public currentUser: firebase.User;
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  async getUserProfile(): Promise<firebase.firestore.DocumentReference> {
+    const user: firebase.User = await this.authService.getUser();
+    this.currentUser = user;
+    this.userProfile = firebase.firestore().doc(`userProfile/${user.uid}`);
+    return this.userProfile;
+  }
+
+  updateName(firstName: string, lastName: string): Promise<void> {
+    return this.userProfile.update({ firstName, lastName });
+  }
+
+  updateDOB(birthDate: string): Promise<any> {
+    return this.userProfile.update({ birthDate });
+  }
+
+  async updateEmail(newEmail: string, password: string): Promise<void> {
+    try {
+      const credential: firebase.auth.AuthCredential = firebase.auth.EmailAuthProvider.credential(
+        this.currentUser.email,
+        password
+      );
+
+      await this.currentUser.reauthenticateWithCredential(credential);
+      await this.currentUser.updateEmail(newEmail);
+      return this.userProfile.update({ email: newEmail });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async updatePassword(
+    newPassword: string,
+    oldPassword: string
+  ): Promise<void> {
+    try {
+      const credential: firebase.auth.AuthCredential = firebase.auth.EmailAuthProvider.credential(
+        this.currentUser.email,
+        oldPassword
+      );
+
+      await this.currentUser.reauthenticateWithCredential(credential);
+      return this.currentUser.updatePassword(newPassword);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
