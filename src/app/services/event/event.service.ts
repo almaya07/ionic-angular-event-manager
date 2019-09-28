3164f81d3e18da6b28f31dae16562c789260ddc4
Jorge Vergara <javebratt@gmail.com>
2019-09-28 16:14:00 -0500

    Adds event service

--- src/app/services/event/event.service.ts
+++ src/app/services/event/event.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import { AuthService } from '../user/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
+  public eventListRef: firebase.firestore.CollectionReference;
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  async createEvent(
+    eventName: string,
+    eventDate: string,
+    eventPrice: number,
+    eventCost: number
+  ): Promise<firebase.firestore.DocumentReference> {
+    const user: firebase.User = await this.authService.getUser();
+    this.eventListRef = firebase
+      .firestore()
+      .collection(`userProfile/${user.uid}/eventList`);
+    return this.eventListRef.add({
+      name: eventName,
+      date: eventDate,
+      price: eventPrice * 1,
+      cost: eventCost * 1,
+      revenue: eventCost * -1
+    });
+  }
+
+  async getEventList(): Promise<firebase.firestore.QuerySnapshot> {
+    const user: firebase.User = await this.authService.getUser();
+    this.eventListRef = firebase
+      .firestore()
+      .collection(`userProfile/${user.uid}/eventList`);
+    return this.eventListRef.get();
+  }
+
+  async getEventDetail(
+    eventId: string
+  ): Promise<firebase.firestore.DocumentSnapshot> {
+    const user: firebase.User = await this.authService.getUser();
+    this.eventListRef = firebase
+      .firestore()
+      .collection(`userProfile/${user.uid}/eventList`);
+    return this.eventListRef.doc(eventId).get();
+  }
 }
