90e0e3dd43f3ff6973d0212e6727630477d8c053
Jorge Vergara <javebratt@gmail.com>
2019-09-28 15:36:25 -0500

    Adds the authentication system

--- src/app/shared-modules/auth.module.ts
+++ src/app/shared-modules/auth.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthFormComponent } from '../components/auth-form/auth-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [AuthFormComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, IonicModule],
   exports: [AuthFormComponent],
   entryComponents: [AuthFormComponent]
 })
