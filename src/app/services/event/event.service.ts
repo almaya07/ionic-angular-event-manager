def076240405f672989686456b061e1fa5be9057
Jorge Vergara <javebratt@gmail.com>
2019-09-28 18:17:06 -0500

    Finishes app

--- src/app/services/event/event.service.ts
+++ src/app/services/event/event.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/storage';
 import { AuthService } from '../user/auth.service';
 
 @Injectable({
@@ -45,5 +46,48 @@ export class EventService {
       .firestore()
       .collection(`userProfile/${user.uid}/eventList`);
     return this.eventListRef.doc(eventId).get();
+  }
+
+  addGuest(
+    guestName: string,
+    eventId: string,
+    eventPrice: number,
+    guestPicture: string = null
+  ): Promise<void> {
+    return this.eventListRef
+      .doc(eventId)
+      .collection('guestList')
+      .add({ guestName })
+      .then(newGuest => {
+        return firebase.firestore().runTransaction(transaction => {
+          return transaction
+            .get(this.eventListRef.doc(eventId))
+            .then(eventDoc => {
+              const newRevenue = eventDoc.data().revenue + eventPrice;
+              transaction.update(this.eventListRef.doc(eventId), {
+                revenue: newRevenue
+              });
+              if (guestPicture != null) {
+                const storageRef = firebase
+                  .storage()
+                  .ref(`/guestProfile/${newGuest.id}/profilePicture.png`);
+
+                return storageRef
+                  .putString(guestPicture, 'base64', {
+                    contentType: 'image/png'
+                  })
+                  .then(() => {
+                    return storageRef.getDownloadURL().then(downloadURL => {
+                      return this.eventListRef
+                        .doc(eventId)
+                        .collection('guestList')
+                        .doc(newGuest.id)
+                        .update({ profilePicture: downloadURL });
+                    });
+                  });
+              }
+            });
+        });
+      });
   }
 }
