00153eaeff14032923a80914850f383fb4335968
Jorge Vergara <javebratt@gmail.com>
2019-09-28 14:52:47 -0500

    Updates to latest versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,27 @@
 import { Component } from '@angular/core';
-import { Plugins } from '@capacitor/core';
 
-import * as firebase from 'firebase/app';
-import { environment } from 'src/environments/environment';
-
-const { SplashScreen, StatusBar } = Plugins;
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor() {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    firebase.initializeApp(environment.firebase);
-    SplashScreen.hide().catch(error => {
-      console.error(error);
-    });
-
-    StatusBar.hide().catch(error => {
-      console.error(error);
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
 }
